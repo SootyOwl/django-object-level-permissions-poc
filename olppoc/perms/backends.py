b2ccecb230edd3e79294fa42b9963966fe725527
Sooty <7614538+SootyOwl@users.noreply.github.com>
2023-07-25 11:34:14 +0100

    style: :art: format with black

--- olppoc/perms/backends.py
+++ olppoc/perms/backends.py
@@ -1,11 +1,12 @@
 """Custom authentication backend for object permissions."""
 
 from collections import defaultdict
+
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
-
 from perms.models import ObjectPermission
 from perms.utils import get_filter_from_constraints, resolve_perm
+
 
 class ObjectPermissionBackend(ModelBackend):
     """Override has_perm and get_all_permissions methods, to check object-level permissions."""
@@ -13,23 +14,27 @@ class ObjectPermissionBackend(ModelBackend):
     def get_all_permissions(self, user_obj, obj=None):
         if not user_obj.is_active or user_obj.is_anonymous:
             return {}
-        if not hasattr(user_obj, '_object_perm_cache') or not user_obj._object_perm_cache:
+        if (
+            not hasattr(user_obj, "_object_perm_cache")
+            or not user_obj._object_perm_cache
+        ):
             # if the user doesn't have an object permission cache, or it's empty,
             # populate it now and cache it on the user object for future use
             user_obj._object_perm_cache = self.get_object_permissions(user_obj)
         return user_obj._object_perm_cache
-    
-    def get_permission_filter(self, user_obj):
-        """Get the permission filter for the user."""
-        return Q(users=user_obj) | Q(groups__user=user_obj)
-    
+
     def get_object_permissions(self, user_obj):
         """Return permissions granted to the user by an ObjectPermission."""
-        objectpermissions = ObjectPermission.objects.filter(
-            self.get_permission_filter(user_obj),
-            enabled=True,
-        ).order_by('id').distinct().prefetch_related('object_types')
-        
+        objectpermissions = (
+            ObjectPermission.objects.filter(
+                self.get_permission_filter(user_obj),
+                enabled=True,
+            )
+            .order_by("id")
+            .distinct()
+            .prefetch_related("object_types")
+        )
+
         perms = defaultdict(list)
         for p in objectpermissions:
             for object_type in p.object_types.all():
@@ -38,7 +43,11 @@ class ObjectPermissionBackend(ModelBackend):
                     perms[perm_name].extend(p.list_constraints())
 
         return perms
-    
+
+    def get_permission_filter(self, user_obj):
+        """Get the permission filter for the user."""
+        return Q(users=user_obj) | Q(groups__user=user_obj)
+
     def has_perm(self, user_obj, perm, obj=None):
         app_label, action, model_name = resolve_perm(perm)
 
@@ -57,7 +66,7 @@ class ObjectPermissionBackend(ModelBackend):
             return True
 
         model = obj._meta.model
-        if model._meta.label_lower != '.'.join((app_label, model_name)):
+        if model._meta.label_lower != ".".join((app_label, model_name)):
             raise ValueError(f"Invalid permission: {perm} for model: {model}")
 
         # compile constraints into a Q object
