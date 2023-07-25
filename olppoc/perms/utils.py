c51fdd3c3b7ee6bdeef7f7037f36cc8cbb32a2e9
Sooty <7614538+SootyOwl@users.noreply.github.com>
2023-07-25 15:47:39 +0100

    feat: :sparkles: add `modify_object_safely` utility function
    intended to be used to prevent a user from modifying objects in ways that would remove their access to the object

--- olppoc/perms/utils.py
+++ olppoc/perms/utils.py
@@ -1,7 +1,9 @@
 """Utility functions for the perms app."""
 
-from django.db.models import Q
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import PermissionDenied
+from django.db import transaction
+from django.db.models import Q
 
 
 def get_filter_from_constraints(constraints: list[dict]) -> Q:
@@ -47,3 +49,40 @@ def resolve_contenttype_from_perm(perm: str) -> tuple[str, str]:
     except ContentType.DoesNotExist as e:
         raise ValueError(f"Cound not find ContentType for: {perm}") from e
     return content_type, action
+
+
+def modify_object_safely(obj, user, update_dict):
+    """
+    Modify an object safely by checking if the user has permission to modify it
+    before and after the modification. Prevents users from modifying objects
+    in ways that would remove their access to the object.
+
+    Args:
+        obj: The object to modify.
+        user: The user attempting to modify the object.
+        update_dict: A dictionary containing the fields to update and their new values.
+
+    Returns:
+        The modified object.
+
+    Raises:
+        PermissionDenied: If the user does not have permission to modify the object.
+    """
+    # validate pre-modified object state
+    queryset = obj.__class__.objects.restrict(user=user, action="change")
+    if not queryset.filter(pk=obj.pk).exists():
+        raise PermissionDenied("You do not have permission to modify this object.")
+    # modify and save the object inside a transaction
+    try:
+        with transaction.atomic():
+            # update the object fields
+            for field, value in update_dict.items():
+                setattr(obj, field, value)
+            obj.save()
+            # check whether the current user can still access the object
+            if not queryset.filter(pk=obj.pk).exists():
+                # abort the transaction
+                raise PermissionDenied("Your changes would have removed your access to this object. Aborting.")
+    except PermissionDenied:
+        raise
+    return obj
